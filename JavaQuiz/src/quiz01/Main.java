64b70d89fc650d24b1fd4cb500bd3550b041065b
jinseok1212 <dlwlstjr110@naver.com>
2024-06-12 17:48:25 +0900

    자바

--- JavaQuiz/src/quiz01/Main.java
+++ JavaQuiz/src/quiz01/Main.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
-		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		
-		int cnt = 0;
-		int day = (c - b - 1) / (a - b) + 1;
-		System.out.println(day);
-		
-		
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+		int[] arr = new int[N];
+		for(int i = 1, j = 0; i <= arr.length; i++,j++) arr[j] = i;
+		for(int j = 0; j < M; j++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			
+			for(int i = a; i <= b; i++) {
+				arr[i-1] = arr[b - i];
+			}
+		}
+		for(int i : arr) {
+			System.out.print(i + " ");
+		}
 	}
     
 }
--- JavaQuiz/src/quiz01/Soultion.java
+++ JavaQuiz/src/quiz01/Soultion.java
@@ -2,9 +2,5 @@ package quiz01;
 
 import java.util.*;
 class Solution {
-    public String[] solution(String myString) {
-    	String[] a = myString.split("x");
-    	Arrays.sort(a);
-		return a;
-    }
+  
 }
